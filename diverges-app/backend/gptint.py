39bccd79ee5d004e58eb4c1a717d0e258a137d97
FabioArao <Fabio.Arao@teya.com>
2024-01-16 14:17:12 +0000

    Adding backend..

--- diverges-app/backend/gptint.py
+++ diverges-app/backend/gptint.py
@@ -2,6 +2,7 @@
 #openai.my_api_key = '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'
 
 #import openai
+from pdfconverter.py import text
 from openai import OpenAI
 client = OpenAI(api_key="")
 
@@ -9,7 +10,7 @@ response = client.chat.completions.create(
   model="gpt-3.5-turbo-1106",
   response_format={ "type": "json_object" },
   messages=[
-    {"role": "system", "content": "who and what he or she does? the following text output JSON."},
+    {"role": "system", "content": " output JSON."},
     {"role": "user", "content": "Fabio Arão Cloud and infraestructures engineer Highly skilled IT Professional with over 15 years of comprehensive experience in Cloud Computing, Infrastructure Management, and Identity and Access Management (IAM). Proven record in leading and implementing diverse high-stakes IT projects. AWS and Azure certified with strong acumen in managing service delivery, support, and project management in large-scale environments. Adept at building and maintaining robust relationships with stakeholders and teams."}
   ]
 )
